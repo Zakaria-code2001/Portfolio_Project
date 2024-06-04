1a4549625ded0c86077bedb0eaf85f9c2dd58751
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-06-04 20:37:31 +0200

    .render

--- client/src/index.js
+++ client/src/index.js
@@ -38,6 +38,9 @@ const App = () => {
                <Route path="/">
                 <HomePage/>
                 </Route> 
+                <Route>
+                        <Redirect to="/" />
+                    </Route>
             </Switch>
 
         </div>
