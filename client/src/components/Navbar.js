920749d084b7ae92730e19d89257be8ddef5091c
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-06-04 21:07:57 +0200

    .redirect

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
-import { useAuth,logout } from '../auth'
-
+import { useAuth, logout } from '../auth'
 
 const LoggedInLinks = () => {
   return (
@@ -10,49 +9,49 @@ const LoggedInLinks = () => {
         <Link className="nav-link active text-white" to="/">Home</Link>
       </li>
       <li className="nav-item">
-          <Link className="nav-link active text-white" to="/Playlists">Playlists</Link>
+        <Link className="nav-link active text-white" to="/Playlists">Playlists</Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link active text-white" to="/" onClick={()=>{logout()}} >Log Out</Link>
+        <Link className="nav-link active text-white" to="/" onClick={() => { logout() }}>Log Out</Link>
       </li>
-      
     </>
   )
 }
 
-const LoggedOutLinks=()=>{
+const LoggedOutLinks = () => {
   return (
     <>
       <li className="nav-item">
-              <Link className="nav-link active text-white" to="/">Home</Link>
-            </li>
-            
-            <li className="nav-item">
-              <Link className="nav-link active text-white" to="/signup">Sign Up</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active text-white" to="/login">Login</Link>
-            </li>
+        <Link className="nav-link active text-white" to="/">Home</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link active text-white" to="/signup">Sign Up</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link active text-white" to="/login">Login</Link>
+      </li>
     </>
   )
 }
 
 const NavBar = () => {
-
   const [logged] = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand text-white" to="/">Views</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" onClick={handleToggleMenu}>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${showMenu ? 'show' : ''}`}>
           <ul className="navbar-nav">
-            
-            {logged?<LoggedInLinks/>:<LoggedOutLinks/>}
-
+            {logged ? <LoggedInLinks /> : <LoggedOutLinks />}
           </ul>
         </div>
       </div>
