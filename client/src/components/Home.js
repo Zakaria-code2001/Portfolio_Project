a91d2266b65fcca93f4cb57124f4bbe5041ce1f9
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-06-05 10:24:05 +0200

    styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,32 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { useAuth } from "../auth";
 
-const LoggedInHome=()=>{
+const LoggedInHome = () => {
     return (
         <div className="list">
             List of yours
+            <Link to='/playlists' className="btn btn-primary">Go to Playlists</Link>
         </div>
     )
 }
 
-const LoggedOutHome=() => {
+const LoggedOutHome = () => {
     return (
-        <div className="home container">
-            <h1 className="heading">welcome to MouZa </h1>
+        <div className="home-container">
+            <h1 className="heading">Welcome to MouZa</h1>
             <Link to='/signup' className="btn btn-primary btn-lg">Get Started</Link>
         </div>
     )
 }
-const HomePage = ()=>{
 
-    const [logged] = useAuth()
-    return(
-        <div>
-            {logged?<LoggedInHome/>:<LoggedOutHome/>}
+const HomePage = () => {
+    const [logged] = useAuth();
+    return (
+        <div className="home-page" style={{backgroundImage: 'url("https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")'}}>
+            {logged ? <LoggedInHome /> : <LoggedOutHome />}
         </div>
-        
     )
 }
-export default HomePage
+
+export default HomePage;
